86bb7d7f8aeba2804e3286c644beb9ad8aeed463
nandimandalamrajeshreddy <rajeshreddy.nandi@gmail.com>
2021-12-14 17:39:45 +0530

    Delete AWS_EC2_CLEANUP_RULE directory

--- AWS_EC2_CLEANUP_RULE/cmp_aws_ec2_data.tf
+++
@@ -1,7 +0,0 @@
-#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
-# Terraform - AWS Data file
-#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
-
-data "aws_sns_topic" "topic" {
-  name = "${var.prefix}-${var.environment}-aws-cost-opt-alert-topic"
-}
